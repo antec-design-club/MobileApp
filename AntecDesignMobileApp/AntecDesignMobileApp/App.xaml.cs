526d655fb10ddf90663d950e6b9eee8409e39baa
Greg Hoeppner <gregh@anteccontrols.com>
2019-06-21 08:02:43 -0500

    Added MQTT connection and message retrieval.

--- AntecDesignMobileApp/AntecDesignMobileApp/App.xaml.cs
+++ AntecDesignMobileApp/AntecDesignMobileApp/App.xaml.cs
@@ -3,6 +3,12 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AntecDesignMobileApp.Services;
 using AntecDesignMobileApp.Views;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Client;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace AntecDesignMobileApp
 {
@@ -17,9 +23,47 @@ namespace AntecDesignMobileApp
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
             // Handle when your app starts
+
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId("App1")
+                .WithTcpServer("test.mosquitto.org")
+                .Build();
+
+            mqttClient.UseConnectedHandler(async _ =>
+            {
+                Debug.WriteLine("MQTT client connected");
+
+                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("/flow-sensor/value").Build());
+
+                Debug.WriteLine("MQTT client subscribed to topic /flow-sensor/value");
+            });
+
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Debug.WriteLine($"MQTT message received: Topic: {e.ApplicationMessage.Topic}, Payload: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            });
+
+            mqttClient.UseDisconnectedHandler(async _ =>
+            {
+                Debug.WriteLine("MQTT client disconnected");
+                await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+
+                try
+                {
+                    await mqttClient.ConnectAsync(options).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"MQTT client failed to reconnect to the server: {ex.Message}");
+                }
+            });
+
+            await mqttClient.ConnectAsync(options).ConfigureAwait(false);
         }
 
         protected override void OnSleep()
